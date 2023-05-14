fd22c0cebfcdcb72e0cfebc974ce50bbd9e8a069
Ayush Barnwal <71561407+Col-Asy@users.noreply.github.com>
2023-05-14 23:13:46 +0530

    CSS Properties
    Gave looks to the site.
    
    Co-Authored-By: Mohak <92783629+satuiro@users.noreply.github.com>

--- pages/calorie.js
+++ pages/calorie.js
@@ -22,7 +22,7 @@ export default function Calorie() {
       });
   }
   return (
-    <>
+    <div className="mt-2 ms-2 me-2 mb-1">
       <form
         className={styles.form}
         onSubmit={(e) => {
@@ -30,61 +30,85 @@ export default function Calorie() {
           handleSubmit(e);
         }}
       >
-        <>
-          <label htmlFor="age">Age: </label>
-          <input type="text" pattern="[0-9]{2}" name="age" id="age" />
-        </>
-        <>
-          <label htmlFor="gender">Gender: </label>
-          <div>
-            <input type="radio" name="gender" value="male" id="male" />
-            <label htmlFor="male">Male</label>
-          </div>
-          <div>
-            <input type="radio" name="gender" value="female" id="female" />
-            <label htmlFor="female">Female</label>
-          </div>
-        </>
-        <>
-          <label htmlFor="height">Height (in cms): </label>
-          <input type="text" name="height" pattern="[0-9]{3}" id="height" />
-        </>
-        <>
-          <label htmlFor="weight">Weight (in kg): </label>
-          <input type="text" name="weight" id="weight" pattern="[0-9]{2,3}" />
-        </>
-        <div>
-          <label htmlFor="diet">Type of diet: </label>
 
-          <input type="radio" name="diet" id="veg" value={"Vegitarian"} />
-          <label htmlFor="veg">Vegitarian</label>
-
-          <input type="radio" name="diet" id="egg" value={"Eggitarian"} />
-          <label htmlFor="egg">Eggitarian</label>
-
-          <input
-            type="radio"
-            name="diet"
-            id="nonVeg"
-            value={"Non Vegetarian"}
-          />
-          <label htmlFor="nonVeg">Non Vegetarian</label>
-
-          <input type="radio" name="diet" id="keto" value={"Keto"} />
-          <label htmlFor="keto">Keto</label>
-
-          <input type="radio" name="diet" id="vegan" value={"Vegan"} />
-          <label htmlFor="vegan">Vegan</label>
+        <div className="input-group input-group-lg display-flex bg-yellow">
+          <span className="input-group-text bg-warning text-272343" id="inputGroup-sizing-lg bg-yellow"><label htmlFor="age"><p className="fs-3 fw-semibold">Age: </p></label></span>
+          <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" pattern="[0-9]{2}" name="age" id="age"></input>
         </div>
-        <>
-          <label htmlFor="allergies">Allergies: </label>
-          <input type="text" name="allergies" id="allergies" />
-        </>
-        <>
-          <button type="submit">Submit</button>
-        </>
+        
+        <div className="display-flex row mt-4">
+          <div className="col-lg-4">
+            <label htmlFor="gender"><p className="fs-3 fw-semibold text-272343 me-0">Gender: </p></label>
+          </div>
+          <div className="input-group-text col-lg-4 bg-warning">   
+            <input type="radio" name="gender" value="male" id="male" class="form-check-input mt-1 " />
+            <label htmlFor="male" className="text-272343 ms-3">Male</label>
+          </div>
+          <div className="input-group-text col-lg-4 bg-warning">
+            <input type="radio" name="gender" value="female" id="female" class="form-check-input mt-1 " />
+            <label htmlFor="female" className="text-272343 ms-3">Female</label>
+          </div> 
+        </div>
+        
+
+        <div className="input-group input-group-lg display-flex mt-4">
+          <span className="input-group-text bg-warning text-272343" id="inputGroup-sizing-lg"><label htmlFor="height"><p className="fs-3 fw-semibold">Height (in cms): </p></label></span>
+          <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" name="height" pattern="[0-9]{3}" id="height" />
+        </div>
+
+        <div className="display-flex mt-4">
+          <div className="input-group input-group-lg">
+            <span className="input-group-text bg-warning text-272343" id="inputGroup-sizing-lg"><label htmlFor="weight"><p className="fs-3 fw-semibold">Weight (in kg): </p></label></span>
+            <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" name="weight" id="weight" pattern="[0-9]{2,3}" />
+          </div>
+        </div>
+
+        <div className="display-flex row mt-4 mb-1">
+          <div className="col-lg-4 mb-1">
+            <label htmlFor="diet"><p className="fs-3 fw-semibold">Type of diet: </p></label>
+          </div>
+          <div className="input-group-text bg-warning text-272343 col-lg-4 mb-1 ">
+            <input type="radio" name="diet" id="veg" value={"Vegitarian"} />
+            <label htmlFor="veg" className="ms-3">Vegitarian</label>
+          </div>
+
+          <div className="input-group-text bg-warning text-272343 col-lg-4 mb-1 ">
+            <input type="radio" name="diet" id="egg" value={"Eggitarian"} />
+            <label htmlFor="egg" className="ms-3">Eggitarian</label>
+          </div>
+          
+          <div className="col-lg-4 mb-1"></div>
+          <div className="input-group-text bg-warning text-272343 col-lg-4 mb-1 pb-4 ">
+            <input type="radio" name="diet" id="nonVeg" value={"Non Vegetarian"} />
+            <label htmlFor="nonVeg" className="ms-3">Non Vegetarian</label>
+          </div>
+
+          <div className="input-group-text bg-warning text-272343 col-lg-4 mb-1 pb-4 ">
+            <input type="radio" name="diet" id="keto" value={"Keto"} />
+            <label htmlFor="keto" className="ms-3">Keto</label>
+          </div>
+
+          <div className="col-lg-4 mb-1"></div>  
+          <div className="input-group-text bg-warning text-272343 col-lg-4 mb-1 pb-4 ">
+            <input type="radio" name="diet" id="vegan" value={"Vegan"} />
+            <label htmlFor="vegan" className="ms-3">Vegan</label>
+          </div>
+          
+        </div>
+        
+        <div className="display-flex mt-4">
+          <div className="input-group input-group-lg">
+            <span className="input-group-text bg-warning text-272343" id="inputGroup-sizing-lg"><label htmlFor="allergies"><p className="fs-3 fw-semibold">Allergies: </p></label></span>
+            <input type="text" className="form-control bg-e3f6f5" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" name="allergies" id="allergies" />
+          </div>
+        </div>
+
+        <div className="text-center mt-5">
+          <button type="submit" class="btn btn-block btn-lg bg-warning text-272343 fw-bold">Submit</button>
+        </div>
+
       </form>
       <div className="response">{!!response && response}</div>
-    </>
+    </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,29 @@
 import Link from "next/link";
 import { Inter } from "next/font/google";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <main>
-        <div>
-          <Link href="/calorie">Calorie Assistant</Link>
+        <div className="row">
+          <div className="col-lg-4 mb-3"></div>
+          <div className="col-lg-4 mb-3">
+            <img src = {require('Images/logo.png')} alt = "logo"/>
+          </div>
+          <div className="col-lg-4 mb-3"></div>
         </div>
-        <div></div>
+        <div className="row justify-content-evenly">
+        <div className="col-4 ms-auto">
+          <Link href="/calorie"><button type="button" class="btn btn-lg btn-outline-warning">Calorie Assistant</button></Link>
+        </div>
+        <div className="col-4 ms-auto">
+          <Link href="/"><button type="button" class="btn btn-lg btn-outline-warning">Medical Assistant</button></Link>
+        </div>
+        </div>
+
       </main>
     </>
   );
