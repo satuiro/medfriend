b18d77d65c8eca5db357c2b18c309b310e6e8394
Mohak <mohakmalhotra008@gmail.com>
2023-05-14 17:12:25 +0530

    integration with openai

--- pages/api/calorie.js
+++ pages/api/calorie.js
@@ -1,4 +1,50 @@
 export default function handler(req, res) {
+
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const config = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+      
+const openai = new OpenAIApi(config);
   const { age, gender, height, weight, diet, allergies } = JSON.parse(req.body);
+  const runPrompt = async () => {
+    
+
+    const prompt = `
+        calculate the bmi where height is ${height} in centimeters and weight is ${weight} also specify the health of person according to the bmi
+        lying in the category of underweight overweight and healthy. Then also calculate the bmr value of the person where height is ${height} in metres, weight is ${weight}, age is ${age}, the diet the 
+        person follows is ${diet}, and the gender is ${gender} also recommend a diet chart for the person according to their bmr value, age and gender. Also recommend a weekly workout and cardio plan for the person and add emoji to the workout,cardio and diet chart response accordingly. 
+        
+        Return response in the following parsable JSON format:
+
+        {
+            "bmi":"bmi_person", 
+            "health":"health_of_person",
+            "bmr":"bmr_person", 
+            "dietchart":"diet_chart_of_person",
+            "workout":"workout_of_person", 
+            "cardio":"cardio_plan"   
+
+        }
+
+    `;
+    
+
+	const response = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: prompt,
+		max_tokens: 2048,
+		temperature: 1,
+	});
+
+	const parsableJSONresponse = response.data.choices[0].text;
+	const parsedResponse = JSON.parse(parsableJSONresponse);
+
+	
+};
+  runPrompt();
   console.log(age, gender, height, weight, diet, allergies);
-}
+  
+};
